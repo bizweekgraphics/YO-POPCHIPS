3888887879916f3fc0d445dc0af41b90efedd3c8
dyep49 <dyep49@gmail.com>
2015-02-04 18:08:16 -0500

    adding install script to handle npm jankery

---
+++ install.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fs = require('fs');
+var glob = require('glob');
+
+new glob.Glob('./**/.npmignore')
+  .on('match', function(path) {
+    fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
+      if (err) throw err;
+    });
+  })
+  .on('error', function(err) {
+    throw err;
+  });
