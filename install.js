dfaf4f51e0585b11a0cc0adea3ef2509780af53f
dyep49 <dyep49@gmail.com>
2015-02-04 18:23:31 -0500

    trying to get this postinstall script to run

--- install.js
+++ install.js
@@ -3,6 +3,8 @@
 var fs = require('fs');
 var glob = require('glob');
 
+console.log('POSTINSTALL SCRIPT')
+
 new glob.Glob('./**/.npmignore')
   .on('match', function(path) {
     fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
