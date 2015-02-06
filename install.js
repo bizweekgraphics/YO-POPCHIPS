843b5bb0fdd19288a7a6f6f2ed15990264733411
dyep49 <dyep49@gmail.com>
2015-02-06 15:22:55 -0500

    test suite runs generator and removes after

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ var glob = require('glob');
 
 console.log('POSTINSTALL SCRIPT')
 
-new glob.Glob('./**/.npmignore')
+new glob.Glob('./generators/**/.npmignore')
   .on('match', function(path) {
     fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
       if (err) throw err;
--- test/generator.spec.js
+++ test/generator.spec.js
@@ -1,17 +1,45 @@
 var path = require('path');
+var fs = require('fs-extra');
 var assert = require('assert');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-generator').assert;
+var should = require('chai').should();
+// var generator = require('./../generators/app')
 
 describe('popchips:app', function() {
-  
+
   describe('when using the default config', function() {
 
-    // before(function(done) {
+    before(function() {
+      this.staticFiles = require('./../generators/app/files.json').staticFiles;
+      this.tempPath = path.join(__dirname + './tmp');
 
-    // })
-    it('something', function() {
-      assert.file(['Gruntfile.js', 'app/router.js', 'app/views/main.js']);
+      this.runGen = helpers.run(__dirname + './../generators/app')
+        .inDir(this.tempPath)
+        .on('ready', function(generator) {
+          generator.npmInstall = function() {
+            console.log('MOCKING NPM INSTALL');
+          }
+
+          generator.bowerInstall = function() {
+            console.log('MOCKING BOWER INSTALL');
+          }
+        })  
+    });
+
+    after(function() {
+      fs.remove(this.tempPath, function(err) {
+        if(err) return console.error(err);
+        console.log('CLEAN UP')
+      })
+    })
+
+    it('contains static files', function(done) {
+      this.runGen.on('end', function() {
+        // assert.files(this.staticFiles)
+        console.log('done');
+        done();
+      })
 
     })
 
